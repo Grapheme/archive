87c95a347a9dd62122befabe9f38fcd4742377cc
sngrl <reserved@mail.ru>
2014-08-13 14:51:15 +0400

    +

---
+++ public/routes.php
@@ -0,0 +1,8 @@
+<?php
+require __DIR__.'/../bootstrap/autoload.php';
+$app = require_once __DIR__.'/../bootstrap/start.php';
+
+$routes = Route::getRoutes();
+foreach($routes as $route) {
+    echo URL::to($route->getPath()) . " <br/>\n";
+}
