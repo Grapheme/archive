9e1c1789d4c0a977a26b48d9989aab3b5df437f4
sngrl <reserved@mail.ru>
2014-08-13 16:31:18 +0400

    +

--- app/modules/dictionaries/views/admin/dics/menu.blade.php
+++ app/modules/dictionaries/views/admin/dics/menu.blade.php
@@ -2,7 +2,7 @@
     $menus = array();
     if (Allow::action($module['group'], 'create')) {
         $menus[] = array(
-            'link' => URL::route('dic.create', null, false),
+            'link' => URL::route('dic.create', null),
             'title' => 'Добавить',
             'class' => 'btn btn-primary'
         );
