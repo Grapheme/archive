cd66d3b764600d3921338c9a626a933aa4a0c6cf
sngrl <reserved@mail.ru>
2014-08-13 16:28:27 +0400

    +

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -3,7 +3,7 @@
 $app = new Illuminate\Foundation\Application;
 $env = $app->detectEnvironment(array(
 	'az' => array('Acer_5742G'),
-	'dev' => array('www.grapheme.ru'),
+	#'production' => array('www.grapheme.ru'),
 ));
 $app->bindInstallPaths(require __DIR__.'/paths.php');
 $framework = $app['path.base'].'/vendor/laravel/framework/src';
