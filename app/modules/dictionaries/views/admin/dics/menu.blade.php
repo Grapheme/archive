0a69ac525a0bf65f1ee76f7396929c6cdbb6b839
sngrl <reserved@mail.ru>
2014-08-13 16:53:11 +0400

    +

--- app/modules/dictionaries/views/admin/dics/menu.blade.php
+++ app/modules/dictionaries/views/admin/dics/menu.blade.php
@@ -11,18 +11,5 @@
     
     <h1>Словари</h1>
 
+    {{ Helper::drawmenu($menus) }}
 
-	<div class="row">
-		<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-			<div class="margin-bottom-25 margin-top-10 ">
-
-                @foreach ($menus as $m => $menu)
-				<a class="{{ $menu['class'] }}" href="{{ $menu['link'] }}">
-                    <? if($_SERVER['REQUEST_URI'] == $menu['link']) { echo '<i class="fa fa-check"></i>'; } ?>
-                    {{ $menu['title'] }}
-                </a>
-                @endforeach
-
-			</div>
-		</div>
-	</div>
