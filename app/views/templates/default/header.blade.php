78b8a683491740de0669414c5b74ea41c9c62e9b
sngrl <reserved@mail.ru>
2014-08-15 14:25:53 +0400

    +

--- app/views/templates/default/header.blade.php
+++ app/views/templates/default/header.blade.php
@@ -2,8 +2,7 @@
 
     <div style="text-align:center; font-size:24px; background-color:#eee; height:100%;">
         <h2 style="margin: 10px auto">Хедер</h2>
-        <a href="/">В начало</a> | 
-        <a href="/feedback">Обратная связь</a>
+        <a href="{{ URL::to('/') }}">В начало</a> 
     </div>
 
 </header>
