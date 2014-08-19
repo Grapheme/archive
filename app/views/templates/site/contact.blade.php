45ee2a04af7fe063e6264f141a74e33f9f6160db
sngrl <reserved@mail.ru>
2014-08-19 18:36:40 +0400

    +

--- app/views/templates/site/contact.blade.php
+++ app/views/templates/site/contact.blade.php
@@ -14,7 +14,18 @@
         <div class="wrapper">
             <h1>Контакты</h1>
 
-            {{ $page->block('content') }}
+            <div class="contacts">
+
+                {{ $page->block('contacts') }}
+    
+                <div class="btn-cont">
+                    <a href="{{ URL::route('page', array('request')) }}" class="us-btn invert">Задать вопрос</a>
+                </div>
+                <div class="contact-hr"></div>
+    
+                {{ $page->block('spec') }}
+
+            </div>
 
         </div>
     </section>
