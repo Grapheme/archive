902f870a2e18332a32dd927203b4ce756f883dae
sngrl <reserved@mail.ru>
2014-08-20 17:01:42 +0400

    news mainpage href fix

--- app/modules/news/views/mainpage.blade.php
+++ app/modules/news/views/mainpage.blade.php
@@ -3,7 +3,7 @@
     	@foreach($news as $new)
         <li>
             <div class="news-date">{{ Helper::rdate("j M Y", strtotime($new->published_at)) }}</div>
-            <a href="{{ URL::route('news_full', array('url' => $new->slug), false) }}" class="us-link">
+            <a href="{{ URL::route('news_full', array('url' => $new->slug)) }}" class="us-link">
                 {{ $new->meta->title }}
             </a>
     	@endforeach
