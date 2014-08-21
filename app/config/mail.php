c9c91003b79a4e0292b8feea811c1fe92a60b33e
sngrl <reserved@mail.ru>
2014-08-21 16:00:35 +0400

    mailjet

--- app/config/mail.php
+++ app/config/mail.php
@@ -8,17 +8,17 @@ return array(
     ),
 
     'driver' => 'smtp',
-    'host' => 'smtp.gmail.com',
+    'host' => 'in.mailjet.com',
     'port' => 587,
     'from' => array(
-        'address' => 'guidetohei@gmail.com',
-        'name' => 'ВидеоГид'
+        'address' => 'support@grapheme.ru',
+        'name' => 'ГКУ РО АДЛС'
     ),
-    'username' => 'guidetohei@gmail.com',
-    'password' => 'PeL5hvBd',
+    'username' => '0d8dd8623bd38b41c43683c41c0558eb',
+    'password' => '465c500abd5f680f0b20405deb967b36',
 
     'sendmail' => '/usr/sbin/sendmail -bs',
     'encryption' => 'tls',
 
-    'pretend' => true,
+    'pretend' => false,
 );
