10ffb9a1f113c73032d7c5257c83775127f64cad
grphm-robot <robot@grapheme.ru>
2014-10-03 00:06:10 +0400

    new fixes

--- app/views/templates/site/footer.blade.php
+++ app/views/templates/site/footer.blade.php
@@ -3,10 +3,16 @@
             <div class="wrapper">
                 <div class="footer-block s-text">
                     &copy; 2014, ГКУ РО «Архив документов<br> по личному составу Ростовской области»
+                    <ul>
+                    	<li><a target="_blank" href="http://cuadaro.donland.ru/">Комитет по управлению архивным делом РО</a></li>
+                    	<li><a target="_blank" href="http://mkrf.ru/">Сайт министерства культуры РФ</a></li>
+                    	<li><a target="_blank" href="http://donland.ru">Официальный портал правительства РО</a></li>
+                    </ul>
                 </div><!--
              --><div class="footer-block">
                     <div class="m-text">Ростов-на-Дону, ул. Соколова 52</div>
-                    <a href="tel:+78632405039" class="phone-link">+7 (863) 240-50-39</a>
+                    <a href="tel:+78632405039" class="phone-link">+7 (863) 240-50-39</a><br>
+                    <i class="stuff-link fa fa-envelope"></i> <a href="http://mail.gkuro-adls.ru" target="_blank" class="stuff-link">Вход для сотрудников</a>
                 </div><!--
              --><div class="footer-block">
                     Приемные часы:<br>
@@ -17,8 +23,7 @@
                 </div><!--
              --><div class="footer-block">
                     Электронная почта:<br>
-                    <a href="mailto:info@gkuro-adls.ru" class="us-link">info@gkuro-adls.ru</a><br>
-                    <i class="stuff-link fa fa-envelope"></i> <a href="http://mail.gkuro-adls.ru" target="_blank" class="stuff-link">Вход для сотрудников</a>
+                    <a href="mailto:info@gkuro-adls.ru" class="us-link">info@gkuro-adls.ru</a>
                 </div>
             </div>
         </footer>
